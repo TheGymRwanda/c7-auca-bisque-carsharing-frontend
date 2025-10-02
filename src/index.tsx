aa12407ba8001e0e54e4dcd18946f01e2809aaec
Clement <bosstclement@gmail.com>
2025-10-02 15:17:39 +0200

    feat(header): finalize Header component

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,15 @@
 import './index.css'
 
 import App from './App'
-import React from 'react'
 import ReactDOM from 'react-dom/client'
+import Header from './components/Header'
+import { BrowserRouter } from 'react-router-dom'
 
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 const root = ReactDOM.createRoot(document.getElementById('root')!)
 root.render(
-  <React.StrictMode>
+  <BrowserRouter>
+    <Header />
     <App />
-  </React.StrictMode>,
+  </BrowserRouter>,
 )
