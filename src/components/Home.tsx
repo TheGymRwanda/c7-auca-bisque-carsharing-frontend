39a6007245c2770a409728887d4296a98133cc28
Eugene Ndizihiwe <eugenekjyn@gmail.com>
2025-10-03 12:05:11 +0200

    feat: about to finish home page

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,15 +1,26 @@
 function Home() {
   return (
     <div>
-      <h1 className="text-4xl">MONI</h1>
-      <h1 className="text-4xl">share</h1>
-      <p className="mt-36">
-        Hello Manuela! <br />
-        What are you up to today?
-      </p>
-      <button className="text-blue mt-12 w-24 bg-white ">book car</button>
-      <button className="text-blue mt-34 w-24 bg-white  ">see my car</button>
-      <button className="text-blue mt-34 w-24 bg-white  ">see my bookings</button>
+      <div>
+        <h1 className="place-self-center text-4xl font-serif">MONI</h1>
+        <h1 className="place-self-center text-4xl font-sans ">share</h1>
+        <p className="mt-12 place-self-center">
+          Hello Manuela! <br />
+          What are you up to today?
+        </p>
+      </div>
+
+      <div className="mt-6 grid grid-cols-1  gap-4">
+        <button className="rounded-3xl bg-white text-blue-600 border border-spacing-10 h-10 ">
+          book car
+        </button>
+        <button className=" mt-34 rounded-3xl  border border-spacing-4 text-blue-400 h-10">
+          see my car
+        </button>
+        <button className=" mt-34 rounded-3xl  border border-spacing-10 text-blue-500 h-10 ">
+          see my bookings
+        </button>
+      </div>
     </div>
   )
 }
