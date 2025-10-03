d10a8405a92dd73e2b908b67568d96513617cb85
Eugene Ndizihiwe <eugenekjyn@gmail.com>
2025-10-03 11:11:55 +0200

    feat: working on home page

---
+++ src/components/Home.tsx
@@ -0,0 +1,17 @@
+function Home() {
+  return (
+    <div>
+      <h1 className="text-4xl">MONI</h1>
+      <h1 className="text-4xl">share</h1>
+      <p className="mt-36">
+        Hello Manuela! <br />
+        What are you up to today?
+      </p>
+      <button className="text-blue mt-12 w-24 bg-white ">book car</button>
+      <button className="text-blue mt-34 w-24 bg-white  ">see my car</button>
+      <button className="text-blue mt-34 w-24 bg-white  ">see my bookings</button>
+    </div>
+  )
+}
+
+export default Home
