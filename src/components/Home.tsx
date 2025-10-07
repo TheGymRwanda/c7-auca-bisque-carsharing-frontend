3129ebcd76866c4f476baeef1eae7daefb844e53
Eugene Ndizihiwe <eugenekjyn@gmail.com>
2025-10-07 12:58:33 +0200

    feat: done with routing to empty pages

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom'
 import positiv from '../assets/positiv.png'
 
 function Home() {
@@ -5,9 +6,9 @@ function Home() {
     <div className="p-4">
       <div className="flex flex-col items-center">
         <div>
-          <img src={positiv} />
+          <img src={positiv} alt="Positive illustration" />
         </div>
-        <p className="mt-8 sm:mt-10 md:mt-12 text-center  sm:text-2xl md:text-3xl max-w-xl px-2">
+        <p className="mt-8 sm:mt-10 md:mt-12 text-center sm:text-2xl md:text-3xl max-w-xl px-2">
           Hello Manuela!
           <br />
           What are you up to today?
@@ -15,18 +16,32 @@ function Home() {
       </div>
 
       <div className="mt-10 sm:mt-14 md:mt-16 max-w-md mx-auto grid grid-cols-1 gap-4 px-4">
-        <button className="rounded-3xl bg-white text-[var(--color-primary)] border border-spacing-10 h-12 font-bold w-full sm:w-full md:w-full px-6 transition-colors duration-300">
+        <Link
+          to="/bookcar"
+          className="flex items-center justify-center rounded-3xl bg-white text-[var(--color-primary)] border border-spacing-10 h-12 font-bold w-full px-6 transition-colors duration-300"
+        >
           Book Car
-        </button>
-        <h1 className="p-5 place-self-center text-lg sm:text-xl md:text-2xl">or</h1>
-        <button className="rounded-3xl border border-spacing-10 hover:bg-white hover:text-[var(--color-primary)] text-white h-12 font-bold w-full sm:w-full md:w-full px-6 transition-colors duration-300">
+        </Link>
+
+        <h1 className="flex justify-center items-center p-5 place-self-center text-lg sm:text-xl md:text-2xl">
+          or
+        </h1>
+
+        <Link
+          to="/seemycar"
+          className="flex justify-center items-center rounded-3xl border border-spacing-10 hover:bg-white hover:text-[var(--color-primary)] text-white h-12 font-bold w-full px-6 transition-colors duration-300 text-center"
+        >
           See My Car
-        </button>
-        <button className="rounded-3xl border border-spacing-10 hover:bg-white hover:text-[var(--color-primary)] text-white h-12 font-bold w-full sm:w-full md:w-full px-6 transition-colors duration-300">
+        </Link>
+        <Link
+          to="/bookings"
+          className="flex justify-center items-center rounded-3xl border border-spacing-10 hover:bg-white hover:text-[var(--color-primary)] text-white h-12 font-bold w-full px-6 transition-colors duration-300 text-center"
+        >
           See My Bookings
-        </button>
+        </Link>
       </div>
     </div>
   )
 }
+
 export default Home
